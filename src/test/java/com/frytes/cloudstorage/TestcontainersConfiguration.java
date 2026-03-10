b264cd736ea288623a2b506073dd4d3d604960dd
Антон <Ryrik17@gmail.com>
2026-03-10 13:28:06 +0300

    fix: refactor testcontainers dynamic properties for Spring Boot 3.5+

--- src/test/java/com/frytes/cloudstorage/TestcontainersConfiguration.java
+++ src/test/java/com/frytes/cloudstorage/TestcontainersConfiguration.java
@@ -3,7 +3,7 @@ package com.frytes.cloudstorage;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
-import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertyRegistrar;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.containers.RabbitMQContainer;
@@ -25,27 +25,34 @@ public class TestcontainersConfiguration {
 
 	@Bean
 	@SuppressWarnings("resource")
-	GenericContainer<?> minioContainer(DynamicPropertyRegistry registry) {
-		GenericContainer<?> container = new GenericContainer<>(DockerImageName.parse("minio/minio:latest"))
+	GenericContainer<?> minioContainer() {
+		return new GenericContainer<>(DockerImageName.parse("minio/minio:latest"))
 				.withExposedPorts(9000)
 				.withEnv("MINIO_ROOT_USER", "minioadmin")
 				.withEnv("MINIO_ROOT_PASSWORD", "minioadmin")
 				.withCommand("server /data");
+	}
 
-		registry.add("minio.url", () -> "http://" + container.getHost() + ":" + container.getMappedPort(9000));
-		registry.add("minio.external-url", () -> "http://" + container.getHost() + ":" + container.getMappedPort(9000));
-
-		return container;
+	@Bean
+	DynamicPropertyRegistrar minioProperties(GenericContainer<?> minioContainer) {
+		return registry -> {
+			registry.add("minio.url", () -> "http://" + minioContainer.getHost() + ":" + minioContainer.getMappedPort(9000));
+			registry.add("minio.external-url", () -> "http://" + minioContainer.getHost() + ":" + minioContainer.getMappedPort(9000));
+		};
 	}
 
 	@Bean
 	@SuppressWarnings("resource")
-	GenericContainer<?> redisContainer(DynamicPropertyRegistry registry) {
-		GenericContainer<?> container = new GenericContainer<>(DockerImageName.parse("redis:7-alpine"))
+	GenericContainer<?> redisContainer() {
+		return new GenericContainer<>(DockerImageName.parse("redis:7-alpine"))
 				.withExposedPorts(6379);
+	}
 
-		registry.add("spring.data.redis.host", container::getHost);
-		registry.add("spring.data.redis.port", () -> container.getMappedPort(6379));
-		return container;
+	@Bean
+	DynamicPropertyRegistrar redisProperties(GenericContainer<?> redisContainer) {
+		return registry -> {
+			registry.add("spring.data.redis.host", redisContainer::getHost);
+			registry.add("spring.data.redis.port", () -> redisContainer.getMappedPort(6379));
+		};
 	}
 }
